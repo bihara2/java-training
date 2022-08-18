4b475598edf484191723913b2ef0f247c8c63ae3
bihara2 <wdbihara@gmail.com>
2022-08-18 16:58:02 +0530

    yield sleep inttrupt examples added

--- Threads/Thread join/src/Application.java
+++ Threads/Thread join/src/Application.java
@@ -4,10 +4,11 @@ public class Application {
         Thread t1=new Thread(emp1);
         t1.start();
 
-        t1.join(2000);
-
         System.out.println("Person A Going to meet Person B");
+        t1.join(2000); //waiting for 2sec until t1 complete
 
+        t1.interrupt(); //interrupt method waits until t1 is going to sleep then only it will interrupt otherwise not
+        t1.yield(); // this means giving a chance to execute to another thread
 
     }
 }
--- Threads/Thread join/src/Employee.java
+++ Threads/Thread join/src/Employee.java
@@ -1,12 +1,21 @@
 public class Employee implements Runnable{
     @Override
     public void run(){
+        try {
+            System.out.println("Person B is going to sleep for 5sec!!");
+            Thread.sleep(5000);
+
+        } catch (InterruptedException e) {
+            //throw new RuntimeException(e);
+            System.out.println("Interrupted!! Person B is start working!!");
+        }
+
         for (int a=5;a>0;a--){
             System.out.println("Person B Finish the work within "+a+"sec");
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                System.out.println("Interrupted during B's Work ");
             }
         }
         System.out.println("Person B Finished the work and met person A");
