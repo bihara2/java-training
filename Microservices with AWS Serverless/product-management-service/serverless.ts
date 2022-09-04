9304940d48fa0d5dd7efa34e958f3f8661532f1b
bihara2 <wdbihara@gmail.com>
2022-09-04 16:27:33 +0530

    setup db

--- Microservices with AWS Serverless/product-management-service/serverless.ts
+++ Microservices with AWS Serverless/product-management-service/serverless.ts
@@ -26,6 +26,12 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
       NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
+      DBHOSTNAME: "product-service.cluster-ro-cksazpqshnoq.us-west-2.rds.amazonaws.com",
+      DBPORT: "5432",
+      DBNAME: "productdb",
+      DBUSERNAME: "postgres",
+      DBPASSWORD: "awsP0stgresPWD",
+      DBSCHEMA: "public",
     },
   },
   // import the function via paths
---
+++ Microservices with AWS Serverless/product-management-service/src/libs/database-manager.ts
@@ -0,0 +1,34 @@
+import { DataSource, EntityManager } from "typeorm";
+
+let datasource: DataSource;
+
+const getDatabaseConnection = async (): Promise<EntityManager> => {
+  if (datasource && datasource.isInitialized) {
+    console.log("Reusing existing database connection");
+    return datasource.manager;
+  } else {
+    console.log("Initializing a new database connection");
+    datasource = new DataSource({
+      applicationName: "product-management-service",
+      type: "postgres",
+      host: process.env.DBHOSTNAME,
+      port: +process.env.DBPORT,
+      database: process.env.DBNAME,
+      username: process.env.DBUSERNAME,
+      password: process.env.DBPASSWORD,
+      schema: process.env.DBSCHEMA,
+      connectTimeoutMS: 30000,
+      synchronize: true,
+      logging: false,
+      useUTC: true,
+      entities: [],
+    });
+
+    return await datasource.initialize().then(() => {
+      console.trace("new database connection is created");
+      return datasource.manager;
+    });
+  }
+};
+
+export { getDatabaseConnection };
