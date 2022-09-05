4744a74af7349009ebbebd88a96c4aa2581f0809
bihara2 <wdbihara@gmail.com>
2022-09-05 16:20:00 +0530

    final code ready to merge

--- Microservices with AWS Serverless/product-management-service/serverless.ts
+++ Microservices with AWS Serverless/product-management-service/serverless.ts
@@ -26,10 +26,10 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
       NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
-      DBHOSTNAME: "",
+      DBHOSTNAME: "your-host-name",
       DBPORT: "5432",
       DBNAME: "productdb",
-      DBUSERNAME: "",
+      DBUSERNAME: "postgres",
       DBPASSWORD: "",
       DBSCHEMA: "public",
     },
