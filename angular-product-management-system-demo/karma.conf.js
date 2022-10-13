14201d963f0e3f54edf11d374e614ca9ed34296f
bihara2 <wdbihara@gmail.com>
2022-10-13 12:46:44 +0530

    new project

--- angular-product-management-system-demo/karma.conf.js
+++ angular-product-management-system-demo/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/demo-app-ems'),
+      dir: require('path').join(__dirname, './coverage/angular-product-management-system-demo'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- angular-product-management-system-demo/src/app/app.component.ts
+++ angular-product-management-system-demo/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'em-root',
+  selector: 'pm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'demo-app-ems';
+  title = 'angular-product-management-system-demo';
 }
