f8e9239519c2e4f04ba5ad23654212d6cc751bcd
bihara2 <wdbihara@gmail.com>
2022-10-13 12:12:02 +0530

    init branch

--- Microservices with AWS Serverless/product-management-service/resources/parameters.sh
+++ Microservices with AWS Serverless/product-management-service/resources/parameters.sh
@@ -4,3 +4,6 @@ aws ssm put-parameter --name /product-management-service/dev/database/pg/databas
 aws ssm put-parameter --name /product-management-service/dev/database/pg/username --value 'postgres' --type String --region us-west-2 --data-type text --profile aws-personal
 aws ssm put-parameter --name /product-management-service/dev/database/pg/password --value 'add db password' --type String --region us-west-2 --data-type text --profile aws-personal
 aws ssm put-parameter --name /product-management-service/dev/database/pg/schema --value 'public' --type String --region us-west-2 --data-type text --profile aws-personal
+
+#run these in console to add environment variables to the aws 
+#onece it is added you dont need to run these commands again and again since it is already stored in aws
